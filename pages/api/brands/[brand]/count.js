8fc0ce70d352de0c886b4a6419582e2ecf0a4267
KC Green <kcgreenn@gmail.com>
2022-02-14 14:38:34 -0500

    Update count.js

--- pages/api/brands/[brand]/count.js
+++ pages/api/brands/[brand]/count.js
@@ -4,12 +4,14 @@ import db from '../../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to find the total number of products with matching brandname
 handler.get(async (req, res) => {
-  await db.connect();
-  const count = await Product.count({ brand: req.query.brand });
+  await db.connect();     // Connect to database
+  // Find total count of products with matching brandname
+  const count = await Product.count({ brand: req.query.brand }); 
 
-  await db.disconnect();
-  res.send({ count: count });
+  await db.disconnect();        // Disconnect from database
+  res.send({ count: count });   // Return total count to requesting client
 });
 
 export default handler;
