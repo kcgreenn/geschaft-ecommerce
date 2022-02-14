806e94b6f719ae9623b92819f85b11e278a25074
KC Green <kcgreenn@gmail.com>
2022-02-14 14:40:41 -0500

    Update [brand].js

--- pages/api/brands/[brand].js
+++ pages/api/brands/[brand].js
@@ -4,15 +4,16 @@ import db from '../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to find products with matching brandname
 handler.get(async (req, res) => {
-  await db.connect();
-  console.log(req.query);
+  await db.connect();       // Connect to database
+  // Return products with matching brandname; skip is passed through the request query parameters
   const products = await Product.find({ brand: req.query.brand }, null, {
     skip: req.query.skip,
     limit: 15,
   });
-  await db.disconnect();
-  res.send(products);
+  await db.disconnect();    // Disconnect from database
+  res.send(products);       // Return matching products in response to client request
 });
 
 export default handler;
