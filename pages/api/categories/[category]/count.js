0e256b78ba317787dafec455f3d8f57ea4bdb300
KC Green <kcgreenn@gmail.com>
2022-02-14 14:44:27 -0500

    Update count.js

--- pages/api/categories/[category]/count.js
+++ pages/api/categories/[category]/count.js
@@ -4,12 +4,14 @@ import db from '../../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to find total count of products in given category
 handler.get(async (req, res) => {
-  await db.connect();
+  await db.connect();     // Connect to the database
+  // Find the total count of products in given category
   const count = await Product.count({ category: req.query.category });
 
-  await db.disconnect();
-  res.send({ count: count });
+  await db.disconnect();      // Disconnect from database
+  res.send({ count: count }); // Return total count in response to client request
 });
 
 export default handler;
