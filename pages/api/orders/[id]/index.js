129b632a44f3d088511170c3b76d8982a245a268
KC Green <kcgreenn@gmail.com>
2022-02-14 14:50:08 -0500

    Update index.js

--- pages/api/orders/[id]/index.js
+++ pages/api/orders/[id]/index.js
@@ -4,12 +4,14 @@ import db from '../../../../utils/db';
 import { isAuth } from '../../../../utils/auth';
 
 const handler = nc();
-handler.use(isAuth);
+handler.use(isAuth);    // Check if user is authenticated
+// API endpoint find order by order ID
 handler.get(async (req, res) => {
-  await db.connect();
+  await db.connect();     // Connect to database
+  // Find order by order ID
   const order = await Order.findById(req.query.id);
-  await db.disconnect();
-  res.send(order);
+  await db.disconnect();    // Disconnect from database
+  res.send(order);      // Return order object in response to client request
 });
 
 export default handler;
