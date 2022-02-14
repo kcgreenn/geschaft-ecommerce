5ac0ec730194c20901550d3df1d2a4061caabe85
KC Green <kcgreenn@gmail.com>
2022-02-14 14:43:15 -0500

    Update [index].js

--- pages/api/categories/[category]/[index].js
+++ pages/api/categories/[category]/[index].js
@@ -4,17 +4,18 @@ import db from '../../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to get products in given category with pagination
 handler.get(async (req, res) => {
-  const limit = 12;
-  await db.connect();
-
+  const limit = 12;     // Limit results to 12 per page
+  await db.connect();   // Connect to database
+  //Get 12 products with matching category starting at index "skip"
   const products = await Product.find({ category: req.query.category }, null, {
     skip: req.query.index * limit,
     limit: limit,
   });
 
-  await db.disconnect();
-  res.send(products);
+  await db.disconnect();  // Disconnect from database
+  res.send(products);     // Return the matching products in the response to the requesting client
   products.length = 0;
 });
 
