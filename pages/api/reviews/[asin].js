a0e85ddb955ff2be8405b843b1d28303e2eae19c
KC Green <kcgreenn@gmail.com>
2022-02-14 15:03:41 -0500

    Update [asin].js

--- pages/api/reviews/[asin].js
+++ pages/api/reviews/[asin].js
@@ -4,14 +4,16 @@ import db from '../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to retrieve reviews for a product; based on ASIN
+// req.query should contain the asin
 handler.get(async (req, res) => {
-  await db.connect();
+  await db.connect();               // Connect to database
+  // Return three revies with given asin
   const reviews = await Review.find({ asin: req.query.asin }, null, {
     limit: 3,
   });
-  await db.disconnect();
-
-  res.send(reviews);
+  await db.disconnect();            // Disconnect from Database
+  res.send(reviews);                // Return array of review objects in response to client request
 });
 
 export default handler;
