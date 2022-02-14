5460762219896cee262bf5b20f9d196000c294fa
KC Green <kcgreenn@gmail.com>
2022-02-14 14:58:56 -0500

    Update [id].js

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -4,11 +4,14 @@ import db from '../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to find product of given product ID
+// req.query should contain the product ID
 handler.get(async (req, res) => {
-  await db.connect();
+  await db.connect();       // Connect to database
+  // Find a product with given product ID
   const product = await Product.findById(req.query.id);
-  await db.disconnect();
-  res.send(product);
+  await db.disconnect();   // Disconnect from database
+  res.send(product);      // Return produst in response to client request
 });
 
 export default handler;
