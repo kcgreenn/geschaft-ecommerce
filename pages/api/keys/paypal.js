e0941e68591894d9d8f839895ed15aa3badc9136
KC Green <kcgreenn@gmail.com>
2022-02-14 14:47:58 -0500

    Update paypal.js

--- pages/api/keys/paypal.js
+++ pages/api/keys/paypal.js
@@ -2,7 +2,8 @@ import nc from 'next-connect';
 import { isAuth } from '../../../utils/auth';
 
 const handler = nc();
-handler.use(isAuth);
+handler.use(isAuth);      // Check if user is authenticated
+// Return Paypal id for Paypal sandbox
 handler.get(async (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
