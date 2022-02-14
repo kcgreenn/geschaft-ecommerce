6010e51eb60d6bb9210c92c3b3490fe90cb68696
KC Green <kcgreenn@gmail.com>
2022-02-14 14:36:58 -0500

    Update [index].js

--- pages/api/brands/[brand]/[index].js
+++ pages/api/brands/[brand]/[index].js
@@ -4,17 +4,19 @@ import db from '../../../../utils/db';
 
 const handler = nc();
 
+// API endpoint for searching brands. Skip is based on [index] * limit
 handler.get(async (req, res) => {
-  const limit = 12;
-  await db.connect();
+  const limit = 12;     // Only return 12 results per page
+  await db.connect();   // Connext to database
 
+  // Find products with matching brand name with skip and limit
   const products = await Product.find({ brand: req.query.brand }, null, {
     skip: req.query.index * limit,
     limit: limit,
   });
 
-  await db.disconnect();
-  res.send(products);
+  await db.disconnect();  // Disconnect from database
+  res.send(products);     // Send products to requesting client
   products.length = 0;
 });
 
