a74fac2b50cb079bcc574bf2ebacea28fc2f1091
KC Green <kcgreenn@gmail.com>
2022-02-14 14:54:03 -0500

    Update pay.js

--- pages/api/orders/[id]/pay.js
+++ pages/api/orders/[id]/pay.js
@@ -7,23 +7,28 @@ import { onError } from '../../../../utils/error';
 const handler = nc({
   onError,
 });
-handler.use(isAuth);
+handler.use(isAuth);      // Check if user is authenticated
+// API endpoint to pay for an order
 handler.put(async (req, res) => {
-  await db.connect();
+  await db.connect();     // Connect to database
+  // Find order by order ID
   const order = await Order.findById(req.query.id);
+  // IF the order is found in database
   if (order) {
-    order.isPaid = true;
-    order.paidAt = Date.now();
-    order.paymentResult = {
+    order.isPaid = true;        // Set paid status to true
+    order.paidAt = Date.now();  // Set paid at data to now
+    order.paymentResult = {     // Save information about order payment
       id: req.body.id,
       status: req.body.status,
       email_address: req.body.email_address,
     };
-    const paidOrder = await order.save();
-    await db.disconnect();
+    const paidOrder = await order.save(); // Save updated order information to database
+    await db.disconnect();                // Disconnect from database
+    // Return order success message in response to client request
     res.send({ message: 'Order paid', order: paidOrder });
   } else {
-    await db.disconnect();
+    await db.disconnect();              // Disconnect from database
+    // Return error message if order could not be fouund
     res.status(404).send({ message: 'Order not found' });
   }
 });
