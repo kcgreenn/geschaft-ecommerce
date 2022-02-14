3e5cdfaa074c7c1fb594417910b6329eef6bdb45
KC Green <kcgreenn@gmail.com>
2022-02-14 15:06:59 -0500

    Update login.js

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -6,12 +6,19 @@ import { signToken } from '../../../utils/auth';
 
 const handler = nc();
 
+// API endpoint to handle user login
+// req.body should contain:
+//    email, password
 handler.post(async (req, res) => {
-  await db.connect();
+  await db.connect();             // Connect to database
+  // Query for user with given email
   const user = await User.findOne({ email: req.body.email });
-  await db.disconnect();
+  await db.disconnect();          // Disconnect from database
+  // Compare the hashed password in the database to the one given by the user
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    // Create a json web token for client authentication
     const token = signToken(user);
+    // Return the token and user information in response to client request
     res.send({
       token,
       _id: user._id,
@@ -24,7 +31,7 @@ handler.post(async (req, res) => {
       country: user.country,
       isAdmin: user.isAdmin,
     });
-  } else {
+  } else {      // If user email is not found or password is inccorect, send error message
     res.status(401).send({ message: 'Invalid Email or Password' });
   }
 });
