c856b96cb256659a5d71a722f55ffd042b0e543b
KC Green <kcgreenn@gmail.com>
2022-02-14 14:46:52 -0500

    Update [category].js

--- pages/api/categories/[category].js
+++ pages/api/categories/[category].js
@@ -4,15 +4,18 @@ import db from '../../../utils/db';
 
 const handler = nc();
 
+// API endpoint to request products in given category
 handler.get(async (req, res) => {
-  await db.connect();
+  await db.connect();     // Connect to database
 
+  // Return all products in given category; limited to 15; skipping first "skip" entries
   const products = await Product.find({ category: req.query.category }, null, {
     skip: req.query.skip,
     limit: 15,
   });
-  await db.disconnect();
-  res.send(products);
+  
+  await db.disconnect();    // Disconnect from database
+  res.send(products);       // Return matching products in response to client request
 });
 
 export default handler;
